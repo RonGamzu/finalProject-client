0c5eb06577ad9689a7ad64247c22ef7768da0f5d
“ronGamzugit config --global user.email “ron3121998@gmail.com <ron3121998@gmail.com>
2021-07-21 02:26:10 +0300

    Navbar hover white

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -36,23 +36,22 @@ function Header({genres, connected, handleLogOut}) {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav id="mainNav" className="ml-auto twxt-warning">
       <GenresDropdown genres={genres}  />
-          <Nav.Link className='navLink' as={Link} to="/allMovies">All Movies</Nav.Link>
+          <Nav.Link className='navLink choose' as={Link} to="/allMovies">All Movies</Nav.Link>
           {!connected &&
             <>
-          <Nav.Link as={Link} to={{pathname:'/logIn'}}>Log in</Nav.Link>
-          <Nav.Link as={Link} to='/signUp'>Sign up</Nav.Link>
+          <Nav.Link className='bold greenLogIn' as={Link} to={{pathname:'/logIn'}}>Log in</Nav.Link>
+          <Nav.Link className='bold signUp' as={Link} to='/signUp'>Sign up</Nav.Link>
           </>
         }
 
           {connected &&(
             <>
-          <Nav.Link as={Link} to='/myReviews'>My Reviews</Nav.Link>
-          <Nav.Link as={Link} to='/addReview'>Add Review</Nav.Link>
-          <Nav.Link as={Link} to='/addMovie'>Add Movie</Nav.Link>
-          <Nav.Link as={Link} to='/signUp'>Profile</Nav.Link>
-          <Nav.Link as={Link} onClick={handleLogOut} to=''>Log out</Nav.Link>
-          
-          <Nav.Link as={Link} to='/signUp' style={{color: 'white'}}>| Hi, {connected[0].user_name}</Nav.Link>
+          <Nav.Link className='choose' as={Link} to='/myReviews'>My Reviews</Nav.Link>
+          <Nav.Link className='choose' as={Link} to='/addReview'>Add Review</Nav.Link>
+          <Nav.Link className='choose' as={Link} to='/addMovie'>Add Movie</Nav.Link>
+          {/* <Nav.Link as={Link} to='/signUp'>Profile</Nav.Link> */}
+          <Nav.Link className='bold choose' as={Link} to='/signUp' style={{color: 'white'}}>| Hi, {connected[0].user_name}</Nav.Link>
+          <Nav.Link className='bold logOut' as={Link} onClick={handleLogOut} to='/'>Log out</Nav.Link>
           </>
           )
           }
